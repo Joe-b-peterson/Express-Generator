16c3d37a443e255b9ed6425ca77989c96934f612
Joe Peterson <josephbpeterson@gmail.com>
2020-06-19 21:12:51 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '728242381277120',
+        clientSecret: 'cb656f9a6ff5da5b1758a7427f28c4d2'
+    }
 }
